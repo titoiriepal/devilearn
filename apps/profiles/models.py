5d5a69f501770bd4dadcfed1e3ec3e2db2c26199
titoiriepal <titoiriepal@hotmail.com>
2026-03-17 11:52:31 +0100

    Models for User and Userporfiles added

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -1,3 +1,4 @@
 from django.db import models
-
+from .models.user import User
+from .models.instructor import InstructorProfile
 # Create your models here.
--- devilearn/settings.py
+++ devilearn/settings.py
@@ -94,16 +94,24 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': '''
+        django.contrib.auth.password_validation.UserAttributeSimilarityValidator
+        ''',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': '''
+        django.contrib.auth.password_validation.MinimumLengthValidator
+        ''',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': '''
+        django.contrib.auth.password_validation.CommonPasswordValidator
+        ''',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': '''
+        django.contrib.auth.password_validation.NumericPasswordValidator
+        ''',
     },
 ]
 
@@ -127,3 +135,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+DEFAUTL_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'profiles.User'
