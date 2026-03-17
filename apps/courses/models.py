38d6bae3d317326e39453879cbd58d273cbbcd33
titoiriepal <titoiriepal@hotmail.com>
2026-03-17 12:06:48 +0100

    Models for Course and CourseCategory created

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -1,3 +1,4 @@
 from django.db import models
-
+from .models.category import Category
+from .models.course import Course, CourseCategory
 # Create your models here.
---
+++ apps/courses/models/course.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.conf import settings
+from .category import Category
+
+
+class Course(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='owned_courses',
+    )
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    overview = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    categories = models.ManyToManyField(Category, related_name='courses')
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"Course: {self.title}"
+
+
+class CourseCategory(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('course', 'category')
+
+    def __str__(self):
+        return f"{self.course.title} - {self.category.name}"
