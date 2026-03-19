31db59cf84e7a6adf6df1f27c70adb71f8e910f6
titoiriepal <titoiriepal@hotmail.com>
2026-03-19 12:21:08 +0100

    Models from Courses an profiles registred in admin

--- apps/courses/admin.py
+++ apps/courses/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
+from .models import (Course, CourseCategory, Review,
+                     Progress, Enrollment, Module, Category)
 
 # Register your models here.
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('name', 'slug')
+    search_fields = ('name',)
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['title', 'overview']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(CourseCategory)
+class CourseCategoryAdmin(admin.ModelAdmin):
+    list_display = ['course', 'category']
+    list_filter = ['category']
+    search_fields = ['course__title']
+
+
+@admin.register(Module)
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'course']
+    list_filter = ['title', 'course__title']
+
+
+@admin.register(Enrollment)
+class EnrollmentAdmin(admin.ModelAdmin):
+    list_display = ['user', 'course', 'enrolled_at']
+    list_filter = ['enrolled_at', 'course']
+    search_fields = ['user__username', 'course__title']
+
+
+@admin.register(Progress)
+class ProgressAdmin(admin.ModelAdmin):
+    list_display = ['user', 'course', 'progress', 'status', 'updated_at']
+    list_filter = ['status']
+    search_fields = ['user__username', 'course__title']
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['user', 'course', 'rating', 'created_at']
+    list_filter = ['rating', 'created_at']
+    search_fields = ['user__username', 'course__title', 'comment']
--- apps/profiles/admin.py
+++ apps/profiles/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, InstructorProfile
 # Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = BaseUserAdmin.fieldsets + \
+        (('Custom Role', {'fields': ('is_instructor',)}),)
+    add_fieldsets = BaseUserAdmin.add_fieldsets + \
+        ((None, {'fields': ('is_instructor',)}),)
+    # list_display = ('username', 'email', 'is_instructor', 'is_staff')
+    # search_fields = ('username', 'email')
+    # list_filter = ('is_instructor', 'is_staff')
+
+
+admin.site.register(InstructorProfile)
