6145836b9b4a02a6cc53565e8408bbbb0e08a53d
titoiriepal <titoiriepal@hotmail.com>
2026-03-19 13:16:30 +0100

    signals.py created to create Instructor profiles register after created a new user if it's instructor

--- apps/profiles/apps.py
+++ apps/profiles/apps.py
@@ -2,4 +2,10 @@ from django.apps import AppConfig
 
 
 class ProfilesConfig(AppConfig):
+    # Enabled by default in Django 3.2+
+    # default_auto_field = 'django.db.models.BigAutoField'
+
     name = 'apps.profiles'
+
+    def ready(self):
+        import apps.profiles.signals  # noqa
